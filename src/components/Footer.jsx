478fe842741d50e60209d1855a8a9442ff2a30cd
fnedic <facunedic@gmail.com>
2024-02-22 14:21:23 -0300

    <Fix>: Footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,37 @@ import { Box, Container, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box sx={{ backgroundColor:"#0f4a8d", color:"#ffffff", height:"5rem", borderRadius:2, mt:4 }}>
-      <Container sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
-        <Typography sx={{ textAlign: "center" }} fontSize={18} fontFamily={"monospace"}>Este es el footer</Typography>
+    <Box
+      sx={{
+        backgroundColor: "#0f4a8d",
+        color: "#ffffff",
+        height: "5rem",
+        borderRadius: 2,
+        mt: 4,
+      }}
+    >
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+        }}
+      >
+        <Typography
+          sx={{ textAlign: "center", color: "#d4ae41" }}
+          fontSize={18}
+          fontFamily={"monospace"}
+        >
+          Website made by{" "}
+          <a
+            href="https://facundo-nedic.zeabur.app/"
+            target="_blank" rel="noopener noreferrer"
+            style={{ color: "#fbce4e", textDecoration: "underline" }}
+          >
+            Facundo Nedic
+          </a>
+        </Typography>
       </Container>
     </Box>
   );
